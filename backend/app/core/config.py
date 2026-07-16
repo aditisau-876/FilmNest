0f7148d7beb2998b904d04100b77c391bb7385a0
aditisau-876 <aditisau8@gmail.com>
2026-07-16 13:50:01 +0530

    Files arranged

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,9 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+from dotenv import load_dotenv
 
+BASE_DIR = Path(__file__).resolve().parents[2]   # adjust if needed
+load_dotenv(BASE_DIR / ".env", override=True)
 
 class Settings(BaseSettings):
     DATABASE_URL: str
